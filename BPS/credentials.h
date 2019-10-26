54c005c2be3d4cab73cd5d54a1425af9c644dd5e
John <jramacrae@gmail.com>
2019-10-26 19:47:18 -0400

    Delete credentials.h

--- BPS/credentials.h
+++
@@ -1,8 +0,0 @@
-
-const char* ssid = "8d1552";
-const char* password = "232129554";
-const char* Host = "BPS32";
-const char* MQ_client = "BPStest";       // your MQTT Client ID
-const char* MQ_user = "jramacrae";//"mosquitto";       // your MQTT password
-const char* MQ_pass = "jram7757";       // your network password
-const char* mqtt_server = "192.168.0.4";
